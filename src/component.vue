c44b03cf44059484094fd1e430b72899fac3ff63
Paul Cotterell <paulcotterell@cexs-mbp.home>
2018-09-20 15:23:05 +0100

    added some logging to see the process of execution

--- src/component.vue
+++ src/component.vue
@@ -1,74 +1,76 @@
 <template lang="html">
-  <input type="tel"
-         :value="formattedValue"
-         @change="change"
-         v-money="{precision, decimal, thousands, prefix, suffix}"
-         class="v-money" />
+    <input type="tel"
+           :value="formattedValue"
+           @change="change"
+           v-money="{precision, decimal, thousands, prefix, suffix}"
+           class="v-money"/>
 </template>
 
 <script>
-import money from './directive'
-import defaults from './options'
-import {format, unformat} from './utils'
+    import money from './directive'
+    import defaults from './options'
+    import {format, unformat} from './utils'
 
-export default {
-  name: 'Money',
-  props: {
-    value: {
-      required: true,
-      type: [Number, String],
-      default: 0
-    },
-    masked: {
-      type: Boolean,
-      default: false
-    },
-    precision: {
-      type: Number,
-      default: () => defaults.precision
-    },
-    decimal: {
-      type: String,
-      default: () => defaults.decimal
-    },
-    thousands: {
-      type: String,
-      default: () => defaults.thousands
-    },
-    prefix: {
-      type: String,
-      default: () => defaults.prefix
-    },
-    suffix: {
-      type: String,
-      default: () => defaults.suffix
-    }
-  },
+    export default {
+        name: 'Money',
+        props: {
+            value: {
+                required: true,
+                type: [Number, String],
+                default: 0
+            },
+            masked: {
+                type: Boolean,
+                default: false
+            },
+            precision: {
+                type: Number,
+                default: () => defaults.precision
+            },
+            decimal: {
+                type: String,
+                default: () => defaults.decimal
+            },
+            thousands: {
+                type: String,
+                default: () => defaults.thousands
+            },
+            prefix: {
+                type: String,
+                default: () => defaults.prefix
+            },
+            suffix: {
+                type: String,
+                default: () => defaults.suffix
+            }
+        },
 
-  directives: {money},
+        directives: {money},
 
-  data () {
-    return {
-      formattedValue: ''
-    }
-  },
+        data() {
+            return {
+                formattedValue: ''
+            }
+        },
 
-  watch: {
-    value: {
-      immediate: true,
-      handler (newValue, oldValue) {
-        var formatted = format(newValue, this.$props)
-        if (formatted !== this.formattedValue) {
-          this.formattedValue = formatted
+        watch: {
+            value: {
+                immediate: true,
+                handler(newValue, oldValue) {
+                    console.log('v-money', newValue, oldValue)
+                    var formatted = format(newValue, this.$props)
+                    if (formatted !== this.formattedValue) {
+                        console.log('v-money', formatted, this.formattedValue)
+                        this.formattedValue = formatted
+                    }
+                }
+            }
+        },
+
+        methods: {
+            change(evt) {
+                this.$emit('input', this.masked ? evt.target.value : unformat(evt.target.value, this.precision))
+            }
         }
-      }
     }
-  },
-
-  methods: {
-    change (evt) {
-      this.$emit('input', this.masked ? evt.target.value : unformat(evt.target.value, this.precision))
-    }
-  }
-}
 </script>
